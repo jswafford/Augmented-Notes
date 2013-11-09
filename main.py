54e62a74e7c3fe72c0c6e6c35f3c402a53600bc1
Joanna Swafford <jes8zv@virginia.edu>
2013-11-08 20:43:47 -0500

    Remove unnecessary comments.

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import os
 import urllib
 import webapp2
@@ -22,8 +8,8 @@ import glob
 import zipfile
 import cStringIO
 import json
+
 # For local testing only, the local runner seems to miss lxml
-sys.path.append("/usr/local/lib/python2.7/site-packages/")
 sys.path.append("/Users/dplepage/.virtualenvs/augnotes/lib/python2.7/site-packages/")
 sys.path.append("/Users/jswafford/.virtualenvs/AugNotes/lib/python2.7/site-packages/")
 import lxml
